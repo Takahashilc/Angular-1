d4139c6befadafbbc474a195a5822eb06aa08edf
takahashilc <ltakahashi0896@gmail.com>
2022-09-28 17:52:34 -0300

    Fim Módulo 1

--- alurapic/src/app/photos/photo-list/photo-list.component.ts
+++ alurapic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 import { Photo } from '../photo/photo';
+
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -12,15 +14,27 @@ export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
   filter: string = '';
 
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
+
   constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private photoServive: PhotoService
   ) {}
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params['userName'];
-    this.photoService
-      .listFromUser(userName)
-      .subscribe((photos) => (this.photos = photos));
+    this.userName = this.activatedRoute.snapshot.params['userName'];
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+  }
+
+  load() {
+    this.photoServive
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe((photos) => {
+        this.filter = '';
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) this.hasMore = false;
+      });
   }
 }
